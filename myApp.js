0a0a9e884da02b3d7746f1c91fc48424856814b2
Tobani Esan-George <108701007+Tobshub@users.noreply.github.com>
2022-10-31 15:36:07 +0000

    completed fCC challenges on basic nodejs and express

--- myApp.js
+++ myApp.js
@@ -1,27 +1,70 @@
 let express = require('express');
 let app = express();
+let bodyParser = require('body-parser');
 
 
-console.log('Hello, world')
+const rootLevelMware = (req, res, next) => {
+  const {method, path, ip} = req;
+  console.log(`${method} ${path} - ${ip}`)
+  next();
+}
+
+app.use(
+  rootLevelMware, 
+  bodyParser.urlencoded({extended: false})
+)
+
+app.get('/', (req,res) => {
+  const absolutePath = `${__dirname}/views/index.html`;
+  res.sendFile(absolutePath);
+  const staticServe = `${__dirname}/public`
+  app.use('/public', express.static(staticServe))
+})
 
 
+app.get('/json', (req, res) => {
+  const {MESSAGE_STYLE: style} = process.env;
+  
+  const mes = 'Hello json';
+  const message = {
+    'message': style === 'uppercase'? mes.toUpperCase() : mes,
+  }
+  
+  res.json(message)
+})
 
+app.get('/now',(req, res, next) => {
+  req.time = new Date().toString();
+  next()
+}, (req, res) => {
+  const response = {
+    time: req.time
+  }
+  res.send(response)
+})
 
+app.get('/:word/echo',(req, res) => {
+  const {word} = req.params;
+  res.send({
+    echo: word
+  })
+})
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+app.route('/name')
+.get((req,res) => {
+  const {first, last} = req.query;
+  const doc = {
+    name: `${first} ${last}`
+  }
+  res.send(doc)
+})
+.post((req,res) => {
+  const {first, last} = req.body;
+  const doc = {
+    name: `${first} ${last}`
+  }
+  res.send(doc)
+})
 
 
 
